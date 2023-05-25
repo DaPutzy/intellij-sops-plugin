0b1a27094ec6c437041b8a09fffe4185f5563064
DaPutzy <9727551+DaPutzy@users.noreply.github.com>
2023-05-25 19:05:13 +0200

    revert accidentally commited intellij version update

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,7 +14,7 @@ repositories {
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-    version.set("2022.3.1")
+    version.set("2022.1")
     type.set("IC") // Target IDE Platform
 
     plugins.set(listOf(/* Plugin Dependencies */))
