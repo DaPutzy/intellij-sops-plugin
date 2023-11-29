bfa562ab1331d63a2d42f0a794aad257f30a3d91
Wassim Vitols <wassim.vitols@gmail.com>
2023-11-29 20:37:18 +1100

    Added "Replace" secret action handler for updating a secret when the private key is not available.
    It works the same as the "Edit" action handler but opens and empty editor window.

---
+++ src/main/java/com/github/daputzy/intellijsopsplugin/handler/ReplaceActionHandler.java
@@ -0,0 +1,62 @@
+package com.github.daputzy.intellijsopsplugin.handler;
+
+import com.github.daputzy.intellijsopsplugin.file.FileUtil;
+import com.github.daputzy.intellijsopsplugin.sops.ExecutionUtil;
+import com.intellij.openapi.application.ApplicationManager;
+import com.intellij.openapi.fileEditor.FileEditorManager;
+import com.intellij.openapi.fileEditor.FileEditorManagerListener;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.VirtualFile;
+import com.intellij.testFramework.LightVirtualFile;
+import com.intellij.util.messages.MessageBusConnection;
+import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
+
+@RequiredArgsConstructor
+public class ReplaceActionHandler {
+
+    @NotNull
+    private final Project project;
+
+    @NotNull
+    private final VirtualFile file;
+
+    public void handle() {
+        final String originalContent = FileUtil.getInstance().getContent(file);
+        final String emptyContent = "";
+        final VirtualFile inMemoryFile = new LightVirtualFile(
+                file.getName(),
+                FileUtil.getInstance().getFileType(file),
+                emptyContent
+        );
+
+        ApplicationManager.getApplication()
+                .invokeLater(() -> FileEditorManager.getInstance(project).openFile(inMemoryFile, true));
+
+        final MessageBusConnection connection = project.getMessageBus().connect();
+        connection.subscribe(FileEditorManagerListener.FILE_EDITOR_MANAGER, new FileEditorManagerListener() {
+            @Override
+            public void fileClosed(@NotNull FileEditorManager source, @NotNull VirtualFile closedFile) {
+                if (inMemoryFile.equals(closedFile)) {
+                    // check if it is our file first, other files may not have a document
+                    final String closedFileContent = FileUtil.getInstance().getDocument(closedFile).getText();
+
+                    if (!closedFileContent.equals(emptyContent)) {
+                        FileUtil.getInstance().writeContentBlocking(file, closedFileContent);
+
+                        ExecutionUtil.getInstance().encrypt(
+                                project,
+                                file,
+                                // success
+                                () -> file.refresh(true, false),
+                                // failure
+                                () -> FileUtil.getInstance().writeContentBlocking(file, originalContent)
+                        );
+
+                        connection.disconnect();
+                    }
+                }
+            }
+        });
+    }
+}
