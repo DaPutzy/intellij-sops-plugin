3f196372c12e27dcd9d5c4debbb40832a16ab178
DaPutzy <9727551+DaPutzy@users.noreply.github.com>
2025-01-15 20:21:20 +0100

    Update maximum IDEA version to 2025.1; prepare releaes

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "com.github.daputzy"
-version = "2.1.1"
+version = "2.1.2"
 
 repositories {
     mavenCentral()
@@ -29,7 +29,7 @@ tasks {
 
     patchPluginXml {
         sinceBuild.set("231.8109.175")
-        untilBuild.set("243.*")
+        untilBuild.set("251.*")
     }
 
     signPlugin {
